490cea95c158c9a728fcf5712f9a24c605ea75dc
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-04-12 17:50:54 +0300

    refactored profile page content to put it in ProfilePageContainer component

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,57 +1,19 @@
 import React from "react";
-import { CustomText } from "@/components/CustomText";
-import { View, StyleSheet, ScrollView } from "react-native";
-import TestingIluustration from "@/assets/illustrations/construction-worker-bro.svg";
-import { CustomLink } from "@/components/Btns/CustomLink";
+import { StyleSheet, ScrollView } from "react-native";
+import ProfilePageContainer from "@/components/containers/ProfilePageContainer";
 
-const ProfileCards = () => {
+const ProfilePage = () => {
   return (
     <ScrollView contentContainerStyle={styles.scrollViewContent}>
-      <View style={styles.container}>
-        <TestingIluustration style={styles.logo}></TestingIluustration>
-        <CustomText type="title">أهلا وسهلا</CustomText>
-
-        <CustomLink
-          href="/"
-          width="xl"
-          color="secondary"
-          label=" هل تريد الإنضام معنا كحرفي؟"
-          title="أنشئ حساب حرفي"
-        />
-        <CustomLink
-          href="/"
-          width="xl"
-          label=" هل تريد الإنضام معنا كمستخدم عادي؟"
-          title="أنشئ حساب عادي"
-        />
-        <CustomLink
-          href="/"
-          width="xl"
-          title="تسجيل الدخول"
-          label="هل تملك حساب؟"
-          color="light"
-        />
-      </View>
+      <ProfilePageContainer />
     </ScrollView>
   );
 };
 
-export default ProfileCards;
+export default ProfilePage;
 
 const styles = StyleSheet.create({
   scrollViewContent: {
     flexGrow: 1,
-  },
-
-  container: {
-    padding: 16,
-    alignItems: "center",
-    marginBottom: 150,
-    marginTop: 50,
-  },
-  logo: {
-    width: 200,
-    height: 200,
-    marginVertical: 20,
   },
 });
---
+++ src/components/containers/ProfilePageBtnsContainer.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { CustomLink } from "@/components/Btns/CustomLink";
+
+type Props = {};
+
+const ProfilePageBtnsContainer = (props: Props) => {
+  return (
+    <>
+      <CustomLink
+        href="/"
+        width="xl"
+        color="secondary"
+        label="profile_tab.do_u_wanna_be_pract"
+        title="profile_tab.create_pract_account"
+      />
+      <CustomLink
+        href="/"
+        width="xl"
+        label="profile_tab.do_u_wanna_be_normal_user"
+        title="profile_tab.create_normal_account"
+      />
+      <CustomLink
+        href="/"
+        width="xl"
+        label="profile_tab.do_u_have_an_account"
+        title="profile_tab.signin"
+        color="light"
+      />
+    </>
+  );
+};
+
+export default ProfilePageBtnsContainer;
