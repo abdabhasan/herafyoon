1f6a9e017e74ac086771a2ad9d23b878d1a287b6
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-04-21 18:17:08 +0300

    created CustomPhoneNumberInput component

---
+++ src/constants/countriesDialCodes.ts
@@ -0,0 +1,16 @@
+const countriesDialCodes = [
+  {
+    key: "JD",
+    name: "country_items.jordan",
+    flag: "🇯🇴",
+    dialCode: "+962",
+  },
+  {
+    key: "PS",
+    name: "country_items.palestine",
+    flag: "🇵🇸",
+    dialCode: "+970",
+  },
+];
+
+export default countriesDialCodes;
