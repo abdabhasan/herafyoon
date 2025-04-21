bdbd13c6974a359922694deac27ce03682f10123
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-04-21 18:23:22 +0300

    created signup form

---
+++ src/components/containers/InputsContainers/index.ts
@@ -0,0 +1,3 @@
+export { default as FullNameFieldsContainer } from "./FullNameInputsContainer";
+export { default as LocationInputsContainer } from "./LocationInputsContainer";
+export { default as EmailAndPasswordFieldsContainer } from "./EmailAndPasswordInputsContainer";
---
+++ src/constants/workTypePickerOptions.ts
@@ -0,0 +1,12 @@
+const workTypePickerOptions = [
+  { label: "work_type_options.joiner", value: "Joiner" },
+  { label: "work_type_options.blacksmith", value: "Blacksmith" },
+  { label: "work_type_options.painter", value: "Painter" },
+  { label: "work_type_options.tile_worker", value: "Tile Worker" },
+  {
+    label: "work_type_options.heavy_truck_driver",
+    value: "Heavy Truck Driver",
+  },
+];
+
+export default workTypePickerOptions;
