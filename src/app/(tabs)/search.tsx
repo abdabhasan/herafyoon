f1642121d648ee2eaee1a8519d5ee108f66bd807
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-05-13 09:40:59 +0300

    added SearchPageContainer to search.tsx

--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,8 +1,19 @@
+import SearchPageContainer from "@/components/containers/PagesContainers/SearchPageContainer";
 import React from "react";
-import { CustomText } from "@/components/CustomText";
+import { StyleSheet, ScrollView } from "react-native";
 
 const ProfileCards = () => {
-  return <CustomText>Search</CustomText>;
+  return (
+    <ScrollView contentContainerStyle={styles.scrollViewContent}>
+      <SearchPageContainer />
+    </ScrollView>
+  );
 };
 
 export default ProfileCards;
+
+const styles = StyleSheet.create({
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+});
