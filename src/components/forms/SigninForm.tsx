9a4055e760509f55f88f98744eb103c91a2755ce
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-05-19 08:00:51 +0300

    implemented login functionality

--- src/components/forms/SigninForm.tsx
+++ src/components/forms/SigninForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ScrollView, StyleSheet } from "react-native";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -7,6 +7,9 @@ import { EmailAndPasswordFieldsContainer } from "@/components/containers/InputsC
 import Toast from "react-native-toast-message";
 import { useTranslation } from "react-i18next";
 import { SiginFormData, signinSchema } from "@/schemas/authSchemas";
+import { loginUser } from "@/firebase/authService";
+import { LoadingSpinner } from "@/components/LoadingSpinner";
+import { useRouter } from "expo-router";
 
 export default function SigninForm() {
   const {
@@ -22,20 +25,50 @@ export default function SigninForm() {
     },
   });
   const { t } = useTranslation();
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
-  const onSubmit = (data: SiginFormData) => {
-    console.log("Form Data:", data);
+  const onSubmit = async (data: SiginFormData) => {
+    setLoading(true);
+    try {
+      const userCredential = await loginUser(data.email, data.password);
 
-    setTimeout(() => {
-      reset();
       Toast.show({
         type: "success",
-        text1: t("signup_page.success"),
-        text2: t("signup_page.will_be_reviewed"),
+        text1: t("signin_page.success"),
         position: "top",
       });
-    }, 1000);
+
+      router.push("/");
+
+      reset();
+    } catch (error: any) {
+      console.error("Login failed:", error);
+
+      if (error?.code === "auth/too-many-requests") {
+        Toast.show({
+          type: "error",
+          text1: t("signin_page.error"),
+          text2: t("signin_page.too_many_requests"),
+          position: "top",
+        });
+      } else {
+        const isAuthError = error?.code === "auth/invalid-credential";
+        Toast.show({
+          type: "error",
+          text1: t("signin_page.error"),
+          text2: isAuthError
+            ? t("signin_page.incorrect_credentials")
+            : error?.message || "Something went wrong. Please try again.",
+          position: "top",
+        });
+      }
+    } finally {
+      setLoading(false);
+    }
   };
+
+  if (loading) return <LoadingSpinner />;
 
   return (
     <ScrollView contentContainerStyle={styles.scrollViewContent}>
