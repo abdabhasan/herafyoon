399d3e8d9ab167e30c7e9c6efd37186ada57d39f
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-05-27 11:51:01 +0300

    added UserInfo type to ProfileInfoCardsContainer

--- src/components/containers/CardsContainers/ProfileInfoCardsContainer.tsx
+++ src/components/containers/CardsContainers/ProfileInfoCardsContainer.tsx
@@ -1,31 +1,39 @@
 import ProfileInfoCard from "@/components/cards/ProfileInfoCard";
 import { CustomText } from "@/components/CustomText";
+import { UserInfo } from "@/types";
 import React from "react";
 
 type Props = {
-  info: any;
+  info: UserInfo | null;
 };
 
 const ProfileInfoCardsContainer: React.FC<Props> = ({ info }) => {
   return (
     <>
-      {info?.userType === "practitioner" && (
-        <ProfileInfoCard
-          name="cards.pract_card.work_type"
-          value={info?.workType}
-        />
+      {info && (
+        <>
+          {info?.workType && info?.userType === "practitioner" && (
+            <ProfileInfoCard
+              name="cards.pract_card.work_type"
+              value={info?.workType ?? ""}
+            />
+          )}
+          <ProfileInfoCard
+            name="cards.pract_card.country"
+            value={info?.country}
+          />
+          <ProfileInfoCard name="cards.pract_card.city" value={info?.city} />
+          <ProfileInfoCard
+            name="cards.pract_card.neighbourhood"
+            value={info?.neighbourhood}
+          />
+          <ProfileInfoCard
+            name="cards.pract_card.phone"
+            value={info?.phoneNumber}
+          />
+          <ProfileInfoCard name="cards.pract_card.email" value={info?.email} />
+        </>
       )}
-      <ProfileInfoCard name="cards.pract_card.country" value={info?.country} />
-      <ProfileInfoCard name="cards.pract_card.city" value={info?.city} />
-      <ProfileInfoCard
-        name="cards.pract_card.neighbourhood"
-        value={info?.neighbourhood}
-      />
-      <ProfileInfoCard
-        name="cards.pract_card.phone"
-        value={info?.phoneNumber}
-      />
-      <ProfileInfoCard name="cards.pract_card.email" value={info?.email} />
       {!info && <CustomText text="no info available" type="error" />}
     </>
   );
