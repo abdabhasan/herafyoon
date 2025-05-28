fc754153fb1264d9bed3e6cd13fd7240c15c4c30
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-05-28 11:22:38 +0300

    moved logout btn to ProfilePageContainer instead of ProfileInfoPageContainer & used logout SVG instead of text

--- src/components/containers/PagesContainers/ProfilePageContainer.tsx
+++ src/components/containers/PagesContainers/ProfilePageContainer.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-import { StyleSheet, ScrollView } from "react-native";
+import { StyleSheet, ScrollView, TouchableOpacity, View } from "react-native";
 import { CustomText } from "@/components/CustomText";
 import { LoadingSpinner } from "@/components/LoadingSpinner";
 import { useAuth } from "@/hooks/useAuth";
 import SigningOptionsPageContainer from "@/components/containers/PagesContainers/SigningOptionsPageContainer";
 import ProfileInfoPageContainer from "@/components/containers/PagesContainers/ProfileInfoPageContainer";
 import LanguageSelectBtn from "@/components/Btns/LanguageSelectBtn";
+import LogoutSVG from "@/assets/illustrations/logout-svgrepo-com.svg";
+import { Colors } from "@/constants/Colors";
 
 type Props = {};
 
 const ProfilePageContainer = (props: Props) => {
-  const { user, loading } = useAuth();
+  const { user, loading, logout } = useAuth();
 
-  console.log("user", user);
+  const handleLogout = async () => {
+    try {
+      await logout();
+      console.log("Successfully logged out");
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
 
   if (loading) {
     return <LoadingSpinner />;
@@ -42,7 +51,12 @@ const ProfilePageContainer = (props: Props) => {
 
   return (
     <ScrollView contentContainerStyle={styles.scrollViewContent}>
-      <LanguageSelectBtn />
+      <View style={styles.btnsContainer}>
+        <LanguageSelectBtn />
+        <TouchableOpacity onPress={handleLogout} style={styles.logoutBtn}>
+          <LogoutSVG width={13} height={13} />
+        </TouchableOpacity>
+      </View>
 
       <ProfileInfoPageContainer />
     </ScrollView>
@@ -56,5 +70,21 @@ const styles = StyleSheet.create({
     flex: 1,
 
     marginBottom: 50,
+  },
+  btnsContainer: {
+    marginTop: 15,
+    paddingHorizontal: 10,
+
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  logoutBtn: {
+    width: 40,
+    height: 40,
+    backgroundColor: Colors.primary.p500,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
