a69f5db9d85eeb4ccf04d100815c5e2c428e20d9
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-06-03 10:55:22 +0300

    added BannerAd to tabs _layout

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from "expo-router";
-import React from "react";
+import React, { useEffect } from "react";
 import { Platform } from "react-native";
 
 import { HapticTab } from "@/components/HapticTab";
@@ -8,53 +8,77 @@ import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useTranslation } from "react-i18next";
 import { TranslationKeys } from "@/i18n/translationKeys";
+import { BannerAd, BannerAdSize } from "react-native-google-mobile-ads";
+
+import mobileAds from "react-native-google-mobile-ads";
 
 export default function TabLayout() {
   const { t } = useTranslation();
 
+  useEffect(() => {
+    mobileAds()
+      .initialize()
+      .then(() => {
+        console.log("AdMob initialized with app ID");
+      });
+  }, []);
+
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors.light.tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: "absolute",
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors.light.tint,
+          headerShown: false,
+          tabBarButton: HapticTab,
+          tabBarBackground: TabBarBackground,
+          tabBarStyle: Platform.select({
+            ios: {
+              // Use a transparent background on iOS to show the blur effect
+              position: "absolute",
+            },
+            default: {},
+          }),
+        }}
+      >
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: t(TranslationKeys.tabs.profile),
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="person.fill" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: t(TranslationKeys.tabs.search),
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="magnifyingglass" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: t(TranslationKeys.tabs.home),
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="house.fill" color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+      <BannerAd
+        unitId={"ca-app-pub-3940256099942544/6300978111"}
+        //  unitId={"ca-app-pub-6809262854669948/6215156572"}
+        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
+          networkExtras: {
+            collapsible: "bottom",
           },
-          default: {},
-        }),
-      }}
-    >
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: t(TranslationKeys.tabs.profile),
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="person.fill" color={color} />
-          ),
         }}
       />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: t(TranslationKeys.tabs.search),
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="magnifyingglass" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: t(TranslationKeys.tabs.home),
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    </>
   );
 }
