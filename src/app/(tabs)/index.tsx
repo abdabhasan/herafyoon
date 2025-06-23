3ec739d660f424fe9ed3c677fa19c43933b56995
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2025-06-23 11:35:15 +0300

    added dynamic backgroun for all index pages using useThemeColor

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { StyleSheet, ScrollView } from "react-native";
 import HomePageContainer from "@/components/containers/PagesContainers/HomePageContainer";
+import { useThemeColor } from "@/hooks/themesHooks/useThemeColor";
 
 export default function Index() {
+  const backgroundColor = useThemeColor({}, "background");
+
   return (
-    <ScrollView contentContainerStyle={styles.scrollViewContent}>
+    <ScrollView
+      contentContainerStyle={[
+        styles.scrollViewContent,
+        {
+          backgroundColor,
+        },
+      ]}
+    >
       <HomePageContainer />
     </ScrollView>
   );
--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { StyleSheet, ScrollView } from "react-native";
 import ProfilePageContainer from "@/components/containers/PagesContainers/ProfilePageContainer";
+import { useThemeColor } from "@/hooks/themesHooks/useThemeColor";
 
 const ProfilePage = () => {
+  const backgroundColor = useThemeColor({}, "background");
+
   return (
-    <ScrollView contentContainerStyle={styles.scrollViewContent}>
+    <ScrollView
+      contentContainerStyle={[
+        styles.scrollViewContent,
+        {
+          backgroundColor,
+        },
+      ]}
+    >
       <ProfilePageContainer />
     </ScrollView>
   );
